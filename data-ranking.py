0f8419ffe8e05dfc37f139499f5ebf6b092192dd
Nathan Krasney <natankrasney@gmail.com>
2021-03-10 11:48:48 +0200

    rankdata+

--- data-ranking.py
+++ data-ranking.py
@@ -2,6 +2,7 @@ from scipy.stats import rankdata
 list = [0, 2, 3, 2]
 print(f'list : {list}')
 
+print(f"rankdata : {rankdata(list)}")
 print(f"rankdata method='min' : {rankdata(list,method='min')}")
 print(f"rankdata method='max' : {rankdata(list,method='max')}")
 print(f"rankdata method='dense' : {rankdata(list,method='dense')}")
